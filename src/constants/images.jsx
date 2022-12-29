be6864f7e01c89a1a26ff1f8ef5d0f88ecc0ce8d
unknown <christian.seguiza@yahoo.com>
2022-12-29 17:45:44 -0500

    Web Portfolio

--- src/constants/images.jsx
+++ src/constants/images.jsx
@@ -1,30 +1,30 @@
-import css from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/css_mk7oue.png';
-import git from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/git_xkwqrt.png';
-import html from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/html_esaapl.png';
-import javascript from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/javascript_uhihwz.png';
-import node from '.https://res.cloudinary.com/djhqargof/image/upload/v1672352471/node_qzczju.png';
-import react from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352472/react_xusziv.png';
-import sass from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352472/sass_nofcgz.png';
+const css = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/css_mk7oue.png'
+const git = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/git_xkwqrt.png';
+const html = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/html_esaapl.png';
+const javascript = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/javascript_uhihwz.png';
+const node = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/node_qzczju.png';
+const react = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352472/react_xusziv.png';
+const sass = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352472/sass_nofcgz.png';
 
-import about01 from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/about01_dytifu.png';
-import about02 from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/about02_ajzqex.png';
-import about03 from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/about03_kkirqg.png';
-import about04 from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/about04_irhfp3.png';
+const about01 = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/about01_dytifu.png';
+const about02 = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/about02_ajzqex.png';
+const about03 = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/about03_kkirqg.png';
+const about04 = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/about04_irhfp3.png';
 
 
-import API from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/API2_vhiyyu.png';
-import bootstrap from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/bootstrap_y7uvmu.png';
-import aboutMePic from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/Business_decisions-bro_ucbyzf.png';
-import newLogo from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/cps-logo_prjdyx.png';
-import developer from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/developer_kutucw.png;
-import ejs from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/ejs_zwhcnq.png';
-import express from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/expressjs_cwyi3g.png';
-import github from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/github_wzhk6t.png';
-import loadBro from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/Load_more-bro_bhdaxs.png';
-import mongodb from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/mongodb_wo7nhg.png';
-import npm from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/npm_ebmwqv.png';
-import photoshop from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/photoshop_xpkm2c.png';
-import vscode from 'https://res.cloudinary.com/djhqargof/image/upload/v1672352472/vscode_xv0ptk.png';
+const API = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/API2_vhiyyu.png';
+const bootstrap = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/bootstrap_y7uvmu.png';
+const aboutMePic = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/Business_decisions-bro_ucbyzf.png';
+const newLogo = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/cps-logo_prjdyx.png';
+const developer = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/developer_kutucw.png';
+const ejs = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/ejs_zwhcnq.png';
+const express = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/expressjs_cwyi3g.png';
+const github = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352470/github_wzhk6t.png';
+const loadBro = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/Load_more-bro_bhdaxs.png';
+const mongodb = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/mongodb_wo7nhg.png';
+const npm = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/npm_ebmwqv.png';
+const photoshop = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352471/photoshop_xpkm2c.png';
+const vscode = 'https://res.cloudinary.com/djhqargof/image/upload/v1672352472/vscode_xv0ptk.png';
 
 export default {
   css,
