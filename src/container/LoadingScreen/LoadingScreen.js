58e166c1fa01d48fac2d5fb1798fdec44511659e
unknown <christian.seguiza@yahoo.com>
2023-03-25 17:24:14 -0400

    Loading Screen Updates

--- src/container/LoadingScreen/LoadingScreen.js
+++ src/container/LoadingScreen/LoadingScreen.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './LoadingScreen.css';
 
 function LoadingScreen() {
-    React.useEffect(() => {
-        const logo = document.querySelector('.logo');
-        setTimeout(() => {
-          logo.style.opacity = '1';
-          logo.style.filter = 'blur(0)';
-          setTimeout(() => {
-            logo.style.opacity = '0';
-            logo.style.filter = 'blur(10px)';
-            setTimeout(() => {
-              document.querySelector('.loading-screen').style.opacity = '0';
-              setTimeout(() => {
-                window.location.href = '/';
-              }, 1000); // Change 1000 to the desired delay in milliseconds
-            }, 1000); // Change 1000 to the desired delay in milliseconds
+  useEffect(() => {
+    const logo = document.querySelector('.logo');
+    let timeout1, timeout2, timeout3, timeout4;
+
+    timeout1 = setTimeout(() => {
+      logo.style.opacity = '1';
+      logo.style.filter = 'blur(0)';
+      timeout2 = setTimeout(() => {
+        logo.style.opacity = '0';
+        logo.style.filter = 'blur(10px)';
+        timeout3 = setTimeout(() => {
+          document.querySelector('.loading-screen').style.opacity = '0';
+          timeout4 = setTimeout(() => {
+            window.location.href = '/';
           }, 1000); // Change 1000 to the desired delay in milliseconds
         }, 1000); // Change 1000 to the desired delay in milliseconds
-      }, []);
-  
-    return (
-      <div className="loading-screen">
-        <img src="https://res.cloudinary.com/djhqargof/image/upload/v1675539397/CPSlogo2_htqw6o.png" alt="logo" className="logo" />
-      </div>
-    );
-  }
+      }, 1000); // Change 1000 to the desired delay in milliseconds
+    }, 1000); // Change 1000 to the desired delay in milliseconds
+
+    // Cleanup function
+    return () => {
+      clearTimeout(timeout1);
+      clearTimeout(timeout2);
+      clearTimeout(timeout3);
+      clearTimeout(timeout4);
+    };
+  }, []);
+
+  return (
+    <div className="loading-screen">
+      <img
+        src="https://res.cloudinary.com/djhqargof/image/upload/v1675539397/CPSlogo2_htqw6o.png"
+        alt="logo"
+        className="logo"
+      />
+    </div>
+  );
+}
 
 export default LoadingScreen;
